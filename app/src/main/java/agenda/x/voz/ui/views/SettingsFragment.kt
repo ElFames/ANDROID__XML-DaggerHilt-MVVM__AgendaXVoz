7a798f2c872a2ea6a8664b59bdf226ba7ddc4b7f
m <m>
2023-09-06 00:11:17 +0200

    dark mode added

--- app/src/main/java/agenda/x/voz/ui/views/SettingsFragment.kt
+++ app/src/main/java/agenda/x/voz/ui/views/SettingsFragment.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import agenda.x.voz.R
-import agenda.x.voz.databinding.FragmentCalendarBinding
 import agenda.x.voz.databinding.FragmentSettingsBinding
-import agenda.x.voz.ui.viewModels.CalendarViewModel
-import androidx.fragment.app.viewModels
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
@@ -22,14 +21,19 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.notificationSettings.setOnClickListener {
-            binding.notificationLabel.visibility = View.VISIBLE
-            binding.notificationSwitch.visibility = View.VISIBLE
-        }
-
         binding.notificationSwitch.setOnClickListener {
             it.isSelected = !it.isSelected
-            binding.notificationLabel.text = if (it.isSelected) "Notificaciones desactivadas" else "Notificaciones Activas"
+            binding.notificationLabel.text = if (it.isSelected) "Notificaciones desactivadas" else "Notificaciones activas"
+        }
+
+        binding.darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                binding.darkModeLabel.text = "Modo noche activado"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                binding.darkModeLabel.text = "Modo noche desativado"
+            }
         }
     }
 }
